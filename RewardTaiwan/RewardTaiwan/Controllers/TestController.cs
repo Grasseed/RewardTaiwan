dca13bd29588221ded9bc61efe0cdbfa02d9b172
小草籽 <44409920+Grasseed@users.noreply.github.com 44409920+Grasseed@users.noreply.github.com>
2024-01-18 01:58:36 +0800

    底層建置: 加入NLog設定

--- RewardTaiwan/RewardTaiwan/Controllers/TestController.cs
+++ RewardTaiwan/RewardTaiwan/Controllers/TestController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using RewardTaiwan.Services;
 using RewardTaiwan.Services.Interface;
 
 namespace RewardTaiwan.Controllers
@@ -8,13 +9,17 @@ namespace RewardTaiwan.Controllers
 	public class TestController : ControllerBase
 	{
 		private readonly ITest _test;
-		public TestController(ITest test)
+		private readonly ILogger<TestController> _logger;
+		public TestController(ITest test, ILogger<TestController> logger)
 		{
 			_test = test;
+			_logger = logger;
 		}
+
 		[HttpGet("GetName")]
 		public string Get(string id)
 		{
+			_logger.Log(LogLevel.Trace, $"Get {id}");
 			return _test.GetName(id);
 		}
 	}
--- RewardTaiwan/RewardTaiwan/Program.cs
+++ RewardTaiwan/RewardTaiwan/Program.cs
@@ -1,5 +1,6 @@
 using Autofac;
 using Autofac.Extensions.DependencyInjection;
+using NLog.Extensions.Logging;
 using RewardTaiwan;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -14,6 +15,17 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// NLog settings
+builder.Services.AddLogging(logging =>
+{
+	//清除原本的 logging provider
+	logging.ClearProviders();
+	//設定 logging 的 minmum level 為 trace
+	logging.SetMinimumLevel(Microsoft.Extensions.Logging.LogLevel.Trace);
+	//使用 NLog 作為 logging provider
+	logging.AddNLog();
+});
 
 var app = builder.Build();
 
