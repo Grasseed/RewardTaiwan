dfdd37aa1b193a47978180ead3b1b6d009d5696f
小草籽 <44409920+Grasseed@users.noreply.github.com 44409920+Grasseed@users.noreply.github.com>
2024-05-05 20:39:42 +0800

    底層建置: 移除無用程式

--- RewardTaiwan/RewardTaiwan/Extensions/ConfigExtension.cs
+++ RewardTaiwan/RewardTaiwan/Extensions/ConfigExtension.cs
@@ -4,14 +4,11 @@
 	{
 		public static void ConnInfoSetting(this IServiceCollection services, IConfiguration config)
 		{
-			//var dbConnections = config.GetSection("DBConnections").Get<List<ConnInfo>>();
 			
 		}
 		public static void ConfigSetting(this IServiceCollection services, IConfiguration config)
 		{
 			//Configs.Aes = config.GetSection("Aes").Get<Configs.AesSetting>();
-			//Configs.Reply = config.GetSection("ReplySetting").Get<Configs.ReplySetting>();
-			//Configs.Skype = config.GetSection("SkypeConfig").Get<Configs.SkypeConfig>();
 			Configs.UseGoogleAuth = config.GetSection("UseGoogleAuthentication").Get<Configs.UseGoogleAuthentication>();
 			Configs.Pages = config.GetSection("SitePages").Get<Configs.SitePages>();
 		}
