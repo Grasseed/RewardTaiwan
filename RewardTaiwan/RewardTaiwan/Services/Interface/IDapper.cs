bd1272e2dfb9720c557630888c7cfed625d870c7
我堆的程式 <115322722+Hu-jay@users.noreply.github.com>
2024-06-16 03:27:19 +0800

    Add files via upload

--- RewardTaiwan/RewardTaiwan/Services/Interface/IDapper.cs
+++ RewardTaiwan/RewardTaiwan/Services/Interface/IDapper.cs
@@ -2,7 +2,8 @@
 {
 	public interface IDapper
 	{
-		T QuerySingle<T>(string sql, object param = null);
+        Task<IEnumerable<FinancialInfo>> GetAllBankNamesAsync();
+        Task<IEnumerable<T>> QueryAsync<T>(string sql, object param = null);
 		IEnumerable<T> Query<T>(string sql, object param = null);
 		int Execute(string sql, object param = null);
 	}
